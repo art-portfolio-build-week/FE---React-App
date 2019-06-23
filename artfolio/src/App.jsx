460f1cf510f305c4234caf758f5c7f8e7cb3a73f
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-06-23 15:38:03 +0200

    connect component to store & fetchApi on mount

--- artfolio/src/App.jsx
+++ artfolio/src/App.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchApi } from "./actions/actionCreators";
 
-function App() {
+function App(props) {
+  const { fetchApi } = props;
+
+  useEffect(() => {
+    fetchApi("https://5d0f7454c56e7600145a42d8.mockapi.io/api/posts");
+  }, []);
+
   return (
     <div className="app">
       <h1>Hello World</h1>
@@ -8,4 +16,12 @@ function App() {
   );
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    postsArray: state.postsArray,
+    isFetching: state.postsArray,
+    errorMessage: state.errorMessage,
+  };
+}
+
+export default connect(mapStateToProps, { fetchApi })(App);
