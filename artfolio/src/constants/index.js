f86aa9a0fff355795007ee21ac135eddef66b167
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-06-24 13:22:55 +0200

    add urls

--- artfolio/src/constants/index.js
+++ artfolio/src/constants/index.js
@@ -1,1 +1,3 @@
-export const loginURL = "https://5d0f7454c56e7600145a42d8.mockapi.io/api/login";
+export const login = "https://5d0f7454c56e7600145a42d8.mockapi.io/api/login";
+export const fetchAll = "https://5d0f7454c56e7600145a42d8.mockapi.io/api/posts";
+export const register = "https://5d0f7454c56e7600145a42d8.mockapi.io/api/posts";
