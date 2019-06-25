87efcc64e7913446c4f92ca7f35bc127343ebd75
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-06-25 13:12:55 +0200

    aadd global styles

--- artfolio/src/App.jsx
+++ artfolio/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { createGlobalStyle } from "styled-components";
 import { connect } from "react-redux";
 import { Route } from "react-router-dom";
 import pt from "prop-types";
@@ -9,6 +10,25 @@ import Register from "./components/Authentication/Register";
 import PostForm from "./components/Posts/Forms/postForm";
 import PostContainer from "./components/Posts/Posts/PostContainer";
 import { fetchAll } from "./constants";
+import lobster from "./assets/fonts/Lobster/Lobster-Regular.ttf";
+
+const GlobalStyle = createGlobalStyle`
+body, html{
+  @font-face {
+    font-family: 'Lobster';
+    src: url(${lobster});
+    font-style: normal;
+    font-weight: 400;
+    font-display: cursive
+  }
+}
+  :root{
+    font-size: 62.5%;
+  }
+  *, *::before, *::after{
+    box-sizing: border-box;
+  }
+`;
 
 function App(props) {
   const { fetchApi } = props;
@@ -19,6 +39,7 @@ function App(props) {
 
   return (
     <div className="app">
+      <GlobalStyle />
       <Header />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
