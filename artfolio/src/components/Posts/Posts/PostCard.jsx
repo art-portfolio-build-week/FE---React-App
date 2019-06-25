25408c28f05dc1c8cf7b05db8da11b0b5ff2861b
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-06-25 11:42:56 +0200

    connect compnent to store & add h1

--- artfolio/src/components/Posts/Posts/PostCard.jsx
+++ artfolio/src/components/Posts/Posts/PostCard.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
+import { postToEdit } from "../../../redux/actions/actionCreators";
 
-export default function PostList(props) {
-  const { description, imgURL } = props.post;
+function PostList(props) {
+  const { postToEdit } = props;
+  const { description, imgURL, username } = props.post;
 
   const onClick = () => {
-    
+    postToEdit(props.post);
   };
 
   return (
     <article>
+      <h1>{username}</h1>
       <img src={imgURL} alt="" />
       <p>{description}</p>
       <button type="button" onClick={onClick}>Edit Post</button>
@@ -17,4 +20,4 @@ export default function PostList(props) {
   );
 }
 
-
+export default connect(state => state, { postToEdit })(PostList);
