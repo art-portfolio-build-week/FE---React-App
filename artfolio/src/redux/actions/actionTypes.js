a42d765a66add4492a44d80f3d9466718ae7e0f6
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-06-25 11:42:35 +0200

    add new action type

--- artfolio/src/redux/actions/actionTypes.js
+++ artfolio/src/redux/actions/actionTypes.js
@@ -4,3 +4,5 @@ export const AUTH_MESSAGE = "AUTH_MESSAGE";
 export const FETCH_API = "FETCH_API";
 export const FETCHING_OK = "FETCHING_OK";
 export const FETCHING_FAIL = "FETCHING_FAIL";
+
+export const POST_TO_EDIT = "POST_TO_EDIT";
