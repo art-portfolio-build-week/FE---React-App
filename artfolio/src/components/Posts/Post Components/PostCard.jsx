cfc8bc5c2903ccc35c7180cb3ed2d184bf1e00da
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-06-26 13:33:09 +0200

    change directory name

---
+++ artfolio/src/components/Posts/Post Components/PostCard.jsx
@@ -0,0 +1,115 @@
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import styled from "styled-components";
+import pt from "prop-types";
+import { postToEdit, editPost, fetchById } from "../../../redux/actions/actionCreators";
+import { putPost, getPostById } from "../../../constants";
+
+const Article = styled.article`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: 30rem;
+  width: 25rem;
+  transition: transform 0.2s;
+  margin: 1.5rem;
+  padding: 1rem;
+  border-radius: 0.5rem;
+  &:hover{
+      transform: scale(1.02);
+      transition: 0.2s;
+      box-shadow: 0 2px 4px 0 rgba(0,0,0,0.5);
+    }
+  img{
+    width: 100%;
+    border-radius: 0.5rem 0.5rem 0 0;
+  }
+  h2{
+    font-family: "lato";
+    font-style: normal;
+    font-size: 1.6rem;
+    font-weight: 500;
+  }
+  button{
+    background-color: white;
+    border: 0.15rem solid black;
+    border-radius: 3.5rem;
+    font-size: 1.5rem;
+    padding: 0.5rem 1rem;
+    transition: 0.2s;
+    cursor: pointer;
+    &:hover{
+      color: white;
+      background-color: black;
+      transition: 0.2s;
+    }
+  }
+`;
+
+function PostList(props) {
+  const { postToEdit, post, fetchById } = props;
+  const { description, imgURL, username, votes, id } = post;
+
+  const [isEditing, updateIsEditing] = useState(false);
+  const [isLiked, updateIsLiked] = useState(false);
+
+  useEffect(() => {
+    updateIsEditing(false);
+  }, [updateIsEditing]);
+
+  const passToState = () => {
+    postToEdit(props.post);
+    updateIsEditing(true);
+  };
+
+  const viewMore = () => {
+    fetchById(getPostById(id));
+  };
+
+  const votePost = () => {
+    if (isLiked) {
+      const unvotedObj = { ...props.post, votes: votes - 1 };
+      editPost(unvotedObj);
+      updateIsLiked(false);
+    }
+    const votedObj = { ...props.post, votes: votes + 1 };
+    editPost(putPost(votedObj.id), votedObj);
+    updateIsLiked(true);
+  };
+
+  if (isEditing) {
+    return <Redirect to="/postart" />;
+  }
+
+  return (
+    <Article>
+      <img src={imgURL} alt="" />
+      <h2>Title: {description}</h2>
+      <h2>Artist: {username}</h2>
+      {/* <button type="button" onClick={passToState}>Edit Post</button>
+      {<button type="button" onClick={votePost}>Vote</button>} */}
+      <button onClick={viewMore} type="button">View More</button>
+    </Article>
+  );
+}
+
+export default connect(state => state, { postToEdit, editPost, fetchById })(PostList);
+
+PostList.defaultProps = {
+  post: {},
+};
+
+PostList.propTypes = {
+  postToEdit: pt.func.isRequired,
+  fetchById: pt.func.isRequired,
+  post: pt.shape({
+    id: pt.number.isRequired,
+    username_id: pt.number.isRequired,
+    description: pt.string.isRequired,
+    imgURL: pt.string.isRequired,
+    votes: pt.number.isRequired,
+    username: pt.string.isRequired,
+  }),
+};
---
+++ artfolio/src/components/Posts/Post Components/PostList.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import pt from "prop-types";
+import styled from "styled-components";
+import PostCard from "./PostCard";
+
+const Div = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-content: space-around;
+  justify-content: center;
+`;
+
+export default function PostList(props) {
+  const { postList } = props;
+  return (
+    <Div>
+      {postList.map(post => <PostCard key={post.id} post={post} />)}
+    </Div>
+  );
+}
+
+PostList.propTypes = {
+  postList: pt.arrayOf(pt.shape({
+    id: pt.number,
+    username_id: pt.number,
+    description: pt.string,
+    imgURL: pt.string,
+    votes: pt.number,
+    username: pt.string,
+  })).isRequired,
+};
