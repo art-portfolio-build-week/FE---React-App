b85384eac4680fa84dd48f78ae0e6c2ea47bbae7
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-06-27 15:28:58 +0200

    add styling

---
+++ artfolio/src/components/Posts/Post Components/Gallery_CSS.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export default styled.ul`
+  display: flex;
+  padding: 5rem;
+  justify-content: center;
+  align-items: center;
+  button{
+    padding: 0.5rem 1.8rem;
+    font-size: 2.4rem;
+    background-color: white;
+    border: 1px solid black;
+    transition: 0.1s ease-in-out;
+    cursor: pointer;
+    &:hover{
+      transition: 0.1s ease-in-out;
+      color: #081826;
+      background-color: #153d5b66;
+    }
+  }
+  .active{
+    background-color: #153d5b66;
+    color: #081826;
+  }
+`;
