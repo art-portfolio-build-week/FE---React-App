4a4017732886d988fe5d51c5d8b1f9359a90bec3
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-06-28 00:48:02 +0200

    add styling for top voted

--- artfolio/src/components/Profile/TopVoted.jsx
+++ artfolio/src/components/Profile/TopVoted.jsx
@@ -1,16 +1,43 @@
 import React from "react";
+import styled from "styled-components";
 import pt from "prop-types";
+import fallback from "../../assets/images/fallBackImage.png";
+
+const Article = styled.article`
+  padding: 0 2rem;
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 2rem;
+  font-family: "latoIta";
+  font-style: italic;
+  img{
+    margin: 0 4rem;
+  }
+  h3{
+    font-family: "lato";
+    font-style: normal;
+  }
+`;
 
 export default function TopVoted({ posts }) {
+  if (posts) {
+    return (
+      <Article>
+        <img src={fallback} alt="nothing to show" />
+        <i>Your highest coted work would appear here.</i>
+        <h3>Title: n/a</h3>
+      </Article>
+    );
+  }
 
   const topPost = posts.sort((a, b) => b.votes - a.votes)[0];
-
   return (
-    <article>
+    <Article>
       <img src={topPost.imgURL} alt={topPost.title} />
       <i>Your most popular work</i>
       <h3>Title: {topPost.title}</h3>
-    </article>
+    </Article>
   );
 }
 
