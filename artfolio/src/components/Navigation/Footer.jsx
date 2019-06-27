2565ca98f3ff5b0e8dcf477e8d8494986cb315d7
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-06-27 19:51:14 +0200

    create footer component

---
+++ artfolio/src/components/Navigation/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+
+
+export default function () {
+  return (
+    <footer>
+      <div>
+        <h4>Company</h4>
+        <p>Privacy Policy</p>
+      </div>
+      <div>
+        <h4>Site Map</h4>
+        <p>Create Account</p>
+        <p>Log In</p>
+      </div>
+      <p>Copyright Artista</p>
+    </footer>
+  );
+}
