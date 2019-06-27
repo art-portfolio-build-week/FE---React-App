142a6b8adae87f73a70a1669a41bb5142721b4e8
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-06-27 19:20:04 -0400

    Adds pagination to Gallery

--- artfolio/src/components/Posts/Post Components/Gallery_CSS.js
+++ artfolio/src/components/Posts/Post Components/Gallery_CSS.js
@@ -5,21 +5,41 @@ export default styled.ul`
   padding: 5rem;
   justify-content: center;
   align-items: center;
-  button{
+  button {
     padding: 0.5rem 1.8rem;
     font-size: 2.4rem;
     background-color: white;
     border: 1px solid black;
     transition: 0.1s ease-in-out;
     cursor: pointer;
-    &:hover{
+    &:hover {
       transition: 0.1s ease-in-out;
       color: #081826;
       background-color: #153d5b66;
     }
   }
-  .active{
+  .active {
     background-color: #153d5b66;
     color: #081826;
   }
+`;
+
+export const Span = styled.span`
+  cursor: pointer;
+  font-size: 1.5rem;
+
+  &.active {
+    font-weight: 800;
+    cursor: initial;
+  }
+`;
+
+export const Pagination = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Divider = styled.span`
+  margin: 0 5px;
+  font-size: 1.5rem;
 `;
