5f734eab914a75cca95decc771f4b8fde6167bff
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-06-27 19:50:33 +0200

    add styling for usercard

--- artfolio/src/components/Profile/UserCard.jsx
+++ artfolio/src/components/Profile/UserCard.jsx
@@ -1,8 +1,34 @@
 import React, { useEffect } from "react";
 import pt from "prop-types";
+import styled from "styled-components";
 import { connect } from "react-redux";
 import { getUserById } from "../../redux/actions/actionCreators";
 import { getUser } from "../../constants";
+
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  line-height: 1.8;
+  border: 2px solid #405768;
+  width: 82rem;
+  height: 251px;
+  box-shadow: 0 2px 4px 0 rgba(0,0,0,0.5);
+  h2{
+    font-size: 2.4rem;
+    font-weight: bold;
+    span{
+      font-weight: normal;
+    }
+    margin-left: 2rem;
+  }
+  p{
+    font-family: "latoIta";
+    font-style: italic;
+    font-weight: 400 !important;
+    margin-left: 1rem;
+  }
+`;
 
 function UserCard(props) {
   const {
@@ -24,11 +50,11 @@ function UserCard(props) {
     return <p>Post does not exist</p>;
   }
   return (
-    <div>
-      <p>{user.uvp}</p>
-      <h1>Contact: <span>{user.user.email}</span></h1>
-      <h1>Name: <span>{user.user.username}</span></h1>
-    </div>
+    <Div>
+      <p>&quot;{user.user.uvp}&quot;</p>
+      <h2>Contact: <span>{user.user.email}</span></h2>
+      <h2>Name: <span>{user.user.username}</span></h2>
+    </Div>
   );
 }
 
