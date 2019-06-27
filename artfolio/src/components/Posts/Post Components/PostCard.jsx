a8a61ed1183078b82e3726dff3eefbdc69cf4b40
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-06-27 13:54:55 +0200

    add liking animation

--- artfolio/src/components/Posts/Post Components/PostCard.jsx
+++ artfolio/src/components/Posts/Post Components/PostCard.jsx
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
-import { Redirect, Link } from "react-router-dom";
-import styled from "styled-components";
+import { Link } from "react-router-dom";
+import styled, { css, keyframes } from "styled-components";
 import pt from "prop-types";
 import { addVotePost, editPost, fetchById } from "../../../redux/actions/actionCreators";
 import { getPostById, votePost } from "../../../constants";
 
 const Article = styled.article`
+  position: relative;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -46,6 +47,73 @@ const Article = styled.article`
       transition: 0.2s;
     }
   }
+  
+`;
+
+const bounceLike = keyframes`
+  40% {
+    -webkit-transform: translateY(-6px);
+            transform: translateY(-6px);
+    -webkit-animation-timing-function: ease-in;
+            animation-timing-function: ease-in;
+  }
+  65% {
+    -webkit-transform: translateY(-12px);
+            transform: translateY(-12px);
+    -webkit-animation-timing-function: ease-in;
+            animation-timing-function: ease-in;
+  }
+  82% {
+    -webkit-transform: translateY(-6px);
+            transform: translateY(-6px);
+    -webkit-animation-timing-function: ease-in;
+            animation-timing-function: ease-in;
+  }
+  93% {
+    -webkit-transform: translateY(-4px);
+            transform: translateY(-4px);
+    -webkit-animation-timing-function: ease-in;
+            animation-timing-function: ease-in;
+  }
+  25%,
+  55%,
+  75%,
+  87% {
+    -webkit-transform: translateY(0px);
+            transform: translateY(0px);
+    -webkit-animation-timing-function: ease-out;
+            animation-timing-function: ease-out;
+  }
+  100% {
+    -webkit-transform: translateY(0px);
+            transform: translateY(0px);
+    -webkit-animation-timing-function: ease-out;
+            animation-timing-function: ease-out;
+    opacity: 1;
+  }
+`;
+
+// throws an error because of vscode typescript linting no idea why but it worksv
+const mixin = css`
+  ${bounceLike}  0.9s both
+`;
+
+const LikeButton = styled.div` 
+    position: absolute;
+    left: 2rem;
+    top: 2rem;
+    animation: ${props => (props.isLiked ? mixin : "none")};
+    color: ${props => (props.isLiked ? "#a9c1d3" : "#f9fafc")};
+    text-shadow: 0rem 0rem 0.3rem #000000;
+    font-size: 2rem;
+    font-family: "Font Awesome 5 Pro";
+    transition: 0.2s;
+    cursor: pointer;
+    &:hover{
+      transition: 0.2s;
+      transform: scale(1.1);
+      color: orange;
+    }
 `;
 
 function PostCard(props) {
@@ -70,12 +138,13 @@ function PostCard(props) {
     updateIsLiked(true);
   };
 
+
   return (
     <Article>
+      <LikeButton type="button" isLiked={isLiked} onClick={votePostByID}>{"\uf164"}</LikeButton>
       <img src={imgURL} alt="" />
       <h2>Title: {title}</h2>
       <h2>Artist: {username}</h2>
-      {<button type="button" onClick={votePostByID}>Vote</button>}
       <Link to={`/post/${id}`}><button onClick={viewMore} type="button">View More</button></Link>
     </Article>
   );
