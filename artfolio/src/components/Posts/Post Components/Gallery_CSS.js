7d8aa16f1c9191b32f2c21daff7e2a6be61ae3e6
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-06-28 02:42:05 +0200

    auto format

--- artfolio/src/components/Posts/Post Components/Gallery_CSS.js
+++ artfolio/src/components/Posts/Post Components/Gallery_CSS.js
@@ -5,20 +5,20 @@ export default styled.ul`
   padding: 5rem;
   justify-content: center;
   align-items: center;
-  button{
+  button {
     padding: 0.5rem 1.8rem;
     font-size: 2.4rem;
     background-color: white;
     border: 1px solid black;
     transition: 0.1s ease-in-out;
     cursor: pointer;
-    &:hover{
+    &:hover {
       transition: 0.1s ease-in-out;
       color: #081826;
       background-color: #153d5b66;
     }
   }
-  .active{
+  .active {
     background-color: #153d5b66;
     color: #081826;
   }
